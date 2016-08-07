fa462e5b948429bc229ec992755902aeb8f29598
Steve Smith <stevesmith2609@gmail.com>
2016-08-07 18:34:43 +0100

    Adds organisation features

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,3 +1,5 @@
 class Organisation < ActiveRecord::Base
+  validates :name, presence: true
+
   has_many :users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :drawings
+  resources :drawings, :organisations
 
   # You can have the root of your site routed with "root"
   root 'drawings#index'
