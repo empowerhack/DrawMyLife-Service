4859480ece50b2c8de247b8a2048dad72e700785
Kriselda Rabino <kriselda.rabino@gmail.com>
2016-08-15 00:52:00 +0100

    Simplify method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
   def destroy
     resource.soft_delete
-    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+
+    sign_out_check
+
     set_flash_message :notice, :destroyed if is_flashing_format?
     yield resource if block_given?
     respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
   end
 
   private
+
+  def sign_out_check
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+  end
 
   def sign_up_params
     params.require(:user).permit(:email, :password, :password_confirmation, :country, :organisation_id)
