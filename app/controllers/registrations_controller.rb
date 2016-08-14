08145cb22e35ad12c75da9c2643eeabe4618ee9a
Kriselda Rabino <kriselda.rabino@gmail.com>
2016-08-15 00:33:42 +0100

    Add deleted_at field to users and enable "deactivation" over hard-deleting users.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
+  def destroy
+    resource.soft_delete
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message :notice, :destroyed if is_flashing_format?
+    yield resource if block_given?
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+  end
+
   private
 
   def sign_up_params
