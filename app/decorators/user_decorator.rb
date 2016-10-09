446b328d3a23140097ca21c9a7bc534ee8f96e4c
Kriselda Rabino <kriselda.rabino@gmail.com>
2016-10-09 23:34:43 +0100

    [95] Add users listing page and link to in nav bar

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,11 +1,19 @@
 class UserDecorator < Draper::Decorator
   delegate_all
 
+  def self.collection_decorator_class
+    PaginatingDecorator
+  end
+
   def email
     if object.deleted_at.present?
       "<em>Deleted user</em>".html_safe
     else
       "<a href=\"mailto:#{object.email}\">#{object.email}</a>".html_safe
     end
+  end
+
+  def organisation_name
+    object.organisation.name
   end
 end
