bb142bab0380e8edbf10468fb212fe5efadf2c11
Kriselda Rabino <kriselda.rabino@gmail.com>
2016-10-31 02:39:27 +0000

    [#95] Remove self-signup and enable super_admins to add users + add user confirmation process. NB: WIP!
    A super_admin now has the ability to manage (view, edit and delete) users. When they create a new user or edit their email address, a user will then receive a confirmation email with a secure link. They will then have to enter a password to proceed. Super_admins can NOT change or view user passwords.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,10 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
