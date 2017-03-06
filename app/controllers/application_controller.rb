2a3e19de190980118b2b429dc35dd5f50519669c
Kriselda Rabino <kriselda.rabino@gmail.com>
2017-03-06 00:07:51 +0000

    [#149] Restrict access to Organisations page to Super Admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authorize_super_admin!
+    unless current_user.super_admin?
+      flash[:error] = "Sorry, you may be a super human being, but you need to be a super admin to do that."
+      redirect_to root_path
+    end
+  end
 end
