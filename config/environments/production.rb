b9df25304c7df2b81260f54dae0de29b971234e0
Kriselda Rabino <kriselda.rabino@gmail.com>
2017-04-01 18:18:55 +0100

    Force SSL for s3 assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,8 @@ Rails.application.configure do
       bucket: ENV.fetch('S3_BUCKET_NAME', ''),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', ''),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', '')
-    }
+    },
+    s3_protocol: :https
   }
 
   # User support email
