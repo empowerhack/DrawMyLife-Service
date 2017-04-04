8bd4deace6718ac56eb1e309579a6be0f69add28
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2017-04-04 07:15:15 +0100

    images now in correct format

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,19 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
- 
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  
+
+  if Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+      config.enable_processing = false
+    end
+  end
+
+ # storage :file
+
 
   # Choose what kind of storage to use for this uploader:
   # storage :fog
@@ -15,6 +24,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+ # def filename
+   # "#{model.class.to_s.underscore}.jpg"
+ # end
 
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -31,27 +43,29 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-
   # Create different versions of your uploaded files:
-   version :thumb do
-     process resize_to_fit: [100, 100]
-     process convert: [:jpg]
-   end
+  version :thumb do
+    process resize_to_fit: [100, 100]
+    process convert: [:jpg]
+  end
 
-   version :medium do
-     process resize_to_fit: [640, 640]
-     process convert: [:jpg]
-   end
+  version :medium do
+    process resize_to_fit: [640, 640]
+    process convert: [:jpg]
+  end
 
-   version :large do
-     process convert: [:jpg]
-   end
+  version :large do
+    process convert: [:jpg]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-   def extension_whitelist
-     %w(jpg jpeg gif png tiff gif bmp pdf)
-   end
+  def extension_whitelist
+    %w(jpg jpeg gif png tiff gif bmp pdf)
+  end
+
+
+
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
