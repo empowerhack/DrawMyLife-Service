505fc2569548b1b5c39492e5c02541e69a2d7450
leungant <leungant@a.com>
2017-06-04 21:08:31 +0200

    HXL stats db schema changes

--- db/migrate/20170601162837_create_hxl_stats_view.rb
+++ db/migrate/20170601162837_create_hxl_stats_view.rb
@@ -1,22 +1,31 @@
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
+class CreateHxlStatsView < ActiveRecord::Migration
+  def up 
+    execute <<-SQL
+      CREATE VIEW hxl_stats_view AS
+      SELECT d.id AS drawing_id, 
+             d.*,
+             -- u.id AS user_id,  -- available from drawings
+             u.country as user_country,
+             o.id AS org_id,  
+             o.name AS org_name
+      FROM drawings d,
+                    users u, 
+                    organisations o
+      WHERE d.user_id = u.id AND
+            u.organisation_id = o.id
+    SQL
+  end
 
-ActiveRecord::Schema.define(version: 20170601162837) do
+  def down
+    execute <<-SQL
+     DROP VIEW IF EXISTS hxl_stats_view
+    SQL
+  end
+end
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "drawings", force: :cascade do |t|
+=begin
+# Schema - for convenience
+ create_table "drawings", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
@@ -66,11 +75,4 @@ ActiveRecord::Schema.define(version: 20170601162837) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-  end
-
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  add_foreign_key "drawings", "users"
-end
+=end
