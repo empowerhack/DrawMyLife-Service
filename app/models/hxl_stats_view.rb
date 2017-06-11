bfb52b787a27252a2ed030f0d43f34e0be4f7c59
leungant <leungant@a.com>
2017-06-11 12:04:04 +0200

    Separate non-class dependent (though very data structure dependent) method from class

--- app/models/hxl_stats_view.rb
+++ app/models/hxl_stats_view.rb
@@ -26,53 +26,53 @@ def hxlstats_tags
    "#affected+children+age_18plus"]
 end
 
+def get_counts_by_gender(hxlstatsmajorgroup)
+  # Group by gender and aggregate
+  gender_totals = Hash.new(0)
+  @hxlstatsgroupsgender = hxlstatsmajorgroup.group_by(&:gender) # shorthand for { |hxlstat| hxlstat.gender }
+  @hxlstatsgroupsgender.each do |k_gender, v_gender|
+    # puts "    cycle genders"
+    # puts "    %s " % k_gender
+    # puts "    %s " % v_gender
+    gender_totals[k_gender] = v_gender.count
+  end
+  gender_totals # hash of key gender to v counts
+end
+
+# rubocop:disable MethodLength
+def get_counts_by_age(hxlstatsmajorgroup)
+  # Processing Age aggregations
+  @hxlstatsgroupsage = hxlstatsmajorgroup.group_by(&:age) # shorthand for { |hxlstat| hxlstat.age }
+  # gender *and* age inclusiveness
+  younger_than_five = 0
+  five_twelve_total = 0
+  thirteen_eighteen_total = 0
+  older_total = 0
+
+  @hxlstatsgroupsage.each do |k_age, _v_age|
+    # k_age is age, _v_age is array of hxlstatsmatview
+    # puts "    cycle ages"
+    # puts "    %s" % k_age
+    # puts "    %s" % _v_age.count
+    if k_age < 5
+      younger_than_five += 1
+    elsif k_age >= 5 && k_age < 13
+      five_twelve_total += 1
+    elsif k_age >= 13 && k_age <= 18
+      thirteen_eighteen_total += 1
+    else
+      older_total += 1
+    end
+  end
+  [younger_than_five, five_twelve_total, thirteen_eighteen_total, older_total]
+end
+
 class HxlStatsView < ActiveRecord::Base
   # View aggregates data in a multi-nested-group by for the HXL proxy/endpoint
   #
   self.table_name = 'hxl_stats_view'
   def readonly?
     true
-  end
-
-  def self.get_counts_by_gender(hxlstatsmajorgroup)
-    # Group by gender and aggregate
-    gender_totals = Hash.new(0)
-    @hxlstatsgroupsgender = hxlstatsmajorgroup.group_by(&:gender) # shorthand for { |hxlstat| hxlstat.gender }
-    @hxlstatsgroupsgender.each do |k_gender, v_gender|
-      # puts "    cycle genders"
-      # puts "    %s " % k_gender
-      # puts "    %s " % v_gender
-      gender_totals[k_gender] = v_gender.count
-    end
-    gender_totals # hash of key gender to v counts
-  end
-
-  # rubocop:disable MethodLength
-  def self.get_counts_by_age(hxlstatsmajorgroup)
-    # Processing Age aggregations
-    @hxlstatsgroupsage = hxlstatsmajorgroup.group_by(&:age) # shorthand for { |hxlstat| hxlstat.age }
-    # gender *and* age inclusiveness
-    younger_than_five = 0
-    five_twelve_total = 0
-    thirteen_eighteen_total = 0
-    older_total = 0
-
-    @hxlstatsgroupsage.each do |k_age, _v_age|
-      # k_age is age, _v_age is array of hxlstatsmatview
-      # puts "    cycle ages"
-      # puts "    %s" % k_age
-      # puts "    %s" % _v_age.count
-      if k_age < 5
-        younger_than_five += 1
-      elsif k_age >= 5 && k_age < 13
-        five_twelve_total += 1
-      elsif k_age >= 13 && k_age <= 18
-        thirteen_eighteen_total += 1
-      else
-        older_total += 1
-      end
-    end
-    [younger_than_five, five_twelve_total, thirteen_eighteen_total, older_total]
   end
 
   def self.hxl_stats_counts
