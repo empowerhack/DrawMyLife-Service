5766dc681ff0d00d9f3671e31293be0d03711edc
leungant <leungant@a.com>
2017-07-15 14:48:58 +0200

    Krissy's input for rubocop errors

--- app/models/hxl_stats_view.rb
+++ app/models/hxl_stats_view.rb
@@ -32,51 +32,49 @@ class HxlStatsView < ActiveRecord::Base
      "#affected+children+age_under5",
      "#affected+children+age_18plus"].freeze
 
-  private_class_method :get_counts_by_gender
-  private_class_method :get_counts_by_age
-
-  def self.get_counts_by_gender(hxlstatsmajorgroup)
-    # Group by gender and aggregate
-    gender_totals = Hash.new(0)
-    hxlstatsgroupsgender = hxlstatsmajorgroup.group_by(&:gender) # shorthand for { |hxlstat| hxlstat.gender }
-    hxlstatsgroupsgender.each do |k_gender, v_gender|
-      # puts "    cycle genders"
-      # puts "    %s " % k_gender
-      # puts "    %s " % v_gender
-      gender_totals[k_gender] = v_gender.count
-    end
-    gender_totals # hash of key gender to v counts
-  end
-
-  # rubocop:disable MethodLength
-  def self.get_counts_by_age(hxlstatsmajorgroup)
-    # Processing Age aggregations
-    hxlstatsgroupsage = hxlstatsmajorgroup.group_by(&:age) # shorthand for { |hxlstat| hxlstat.age }
-    # gender *and* age inclusiveness
-    younger_than_five = 0
-    five_twelve_total = 0
-    thirteen_eighteen_total = 0
-    older_total = 0
-
-    hxlstatsgroupsage.each do |k_age, _v_age|
-      # k_age is age, _v_age is array of hxlstatsmatview
-      # puts "    cycle ages"
-      # puts "    %s" % k_age
-      # puts "    %s" % _v_age.count
-      if k_age < 5
-        younger_than_five += 1
-      elsif k_age >= 5 && k_age < 13
-        five_twelve_total += 1
-      elsif k_age >= 13 && k_age <= 18
-        thirteen_eighteen_total += 1
-      else
-        older_total += 1
+  class << self
+    def self.get_counts_by_gender(hxlstatsmajorgroup)
+      # Group by gender and aggregate
+      gender_totals = Hash.new(0)
+      hxlstatsgroupsgender = hxlstatsmajorgroup.group_by(&:gender) # shorthand for { |hxlstat| hxlstat.gender }
+      hxlstatsgroupsgender.each do |k_gender, v_gender|
+        # puts "    cycle genders"
+        # puts "    %s " % k_gender
+        # puts "    %s " % v_gender
+        gender_totals[k_gender] = v_gender.count
       end
+      gender_totals # hash of key gender to v counts
     end
-    [younger_than_five, five_twelve_total, thirteen_eighteen_total, older_total]
+
+    # rubocop:disable MethodLength
+    def self.get_counts_by_age(hxlstatsmajorgroup)
+      # Processing Age aggregations
+      hxlstatsgroupsage = hxlstatsmajorgroup.group_by(&:age) # shorthand for { |hxlstat| hxlstat.age }
+      # gender *and* age inclusiveness
+      younger_than_five = 0
+      five_twelve_total = 0
+      thirteen_eighteen_total = 0
+      older_total = 0
+
+      hxlstatsgroupsage.each do |k_age, _v_age|
+        # k_age is age, _v_age is array of hxlstatsmatview
+        # puts "    cycle ages"
+        # puts "    %s" % k_age
+        # puts "    %s" % _v_age.count
+        if k_age < 5
+          younger_than_five += 1
+        elsif k_age >= 5 && k_age < 13
+          five_twelve_total += 1
+        elsif k_age >= 13 && k_age <= 18
+          thirteen_eighteen_total += 1
+        else
+          older_total += 1
+        end
+      end
+      [younger_than_five, five_twelve_total, thirteen_eighteen_total, older_total]
+    end
   end
 
-  # rubocop:disable MethodLength
   def self.hxl_stats_counts
     results = []
     hxlstats = HxlStatsView.all
--- db/migrate/20170601162837_create_hxl_stats_view.rb
+++ db/migrate/20170601162837_create_hxl_stats_view.rb
@@ -2,13 +2,13 @@ class CreateHxlStatsView < ActiveRecord::Migration
   def up 
     execute <<-SQL
       CREATE VIEW hxl_stats_view AS
-      SELECT d.id AS drawing_id, 
+      SELECT d.id AS drawing_id,
              d.*,
              u.country as user_country,
-             o.id AS org_id,  
+             o.id AS org_id,
              o.name AS org_name
       FROM drawings d,
-                    users u, 
+                    users u,
                     organisations o
       WHERE d.user_id = u.id AND
             u.organisation_id = o.id
